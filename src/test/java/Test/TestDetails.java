2210d7cec7c43a4f9b2951c9f42c222929ce078a
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-04-25 01:14:49 +0530

    Added File IO Program

---
+++ src/test/java/Test/TestDetails.java
@@ -0,0 +1,15 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+	
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }	
+	   
+}
